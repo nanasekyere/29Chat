34250a49021cf9d2898dd2343ce97a0edd6933e7
nanasekyere <aaronny13@icloud.com>
2026-02-17 22:36:22 +0000

    validator fix

--- services/auth-service/src/validators/auth.validator.ts
+++ services/auth-service/src/validators/auth.validator.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 
 export const authSchema = z.object({
   email: z.email(),
+  name: z.string().min(2).max(50),
   password: z.string().min(8),
 });
 
--- services/auth-service/src/validators/user.validator.ts
+++ services/auth-service/src/validators/user.validator.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 export const userSchema = z.object({
   email: z.email(),
-  name: z.string().min(2).max(255),
+  name: z.string().min(2).max(50),
   avatar: z.string().max(255).optional(),
   status: z.enum(["online", "offline", "away", "busy"]).optional(),
 });
