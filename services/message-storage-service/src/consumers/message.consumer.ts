505cb556ad1c296c778811c31aaa666cf969a78e
nanasekyere <aaronny13@icloud.com>
2026-03-05 23:01:20 +0000

    refactor: simplify message parsing in the message consumer

--- services/message-storage-service/src/consumers/message.consumer.ts
+++ services/message-storage-service/src/consumers/message.consumer.ts
@@ -1,6 +1,6 @@
 import amqplib, { Channel, ChannelModel, ConsumeMessage } from "amqplib";
 import { createMessage } from "../services/message.service";
-import { AppError, MessageContentType } from "@29chat/common";
+import { AppError, NewMessage } from "@29chat/common";
 import { logger } from "../config/logger";
 
 const RABBITMQ_URL = process.env.RABBITMQ_URL || "amqp://localhost:5672";
@@ -20,13 +20,9 @@ export async function startConsumer() {
     if (!msg) return;
 
     try {
-      const msgContent = JSON.parse(msg.content.toString());
-      const senderId = msgContent.senderId as string;
-      const roomId = msgContent.roomId as string;
-      const content = msgContent.content as string;
-      const contentType = msgContent.contentType as MessageContentType;
+      const newMsg = JSON.parse(msg.content.toString()) as NewMessage;
 
-      await createMessage({ senderId, roomId, content, contentType });
+      await createMessage(newMsg);
 
       channel.ack(msg);
     } catch (error) {
