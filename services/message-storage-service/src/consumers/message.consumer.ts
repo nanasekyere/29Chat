4013e51c5b606094d2a634ae1b70415446e98672
nanasekyere <aaronny13@icloud.com>
2026-03-05 22:58:48 +0000

    fix: improve code formatting and consistency in message consumer

--- services/message-storage-service/src/consumers/message.consumer.ts
+++ services/message-storage-service/src/consumers/message.consumer.ts
@@ -3,8 +3,7 @@ import { createMessage } from "../services/message.service";
 import { AppError, MessageContentType } from "@29chat/common";
 import { logger } from "../config/logger";
 
-
-const RABBITMQ_URL = process.env.RABBITMQ_URL || 'amqp://localhost:5672'
+const RABBITMQ_URL = process.env.RABBITMQ_URL || "amqp://localhost:5672";
 const QUEUE = "messages.new";
 
 let connection: ChannelModel;
@@ -14,11 +13,11 @@ export async function startConsumer() {
   connection = await amqplib.connect(RABBITMQ_URL);
   channel = await connection.createChannel();
 
-  await channel.assertQueue(QUEUE, {durable: true});
+  await channel.assertQueue(QUEUE, { durable: true });
   channel.prefetch(1);
 
   channel.consume(QUEUE, async (msg: ConsumeMessage | null) => {
-    if (!msg) return
+    if (!msg) return;
 
     try {
       const msgContent = JSON.parse(msg.content.toString());
@@ -27,20 +26,17 @@ export async function startConsumer() {
       const content = msgContent.content as string;
       const contentType = msgContent.contentType as MessageContentType;
 
-      await createMessage({senderId, roomId, content, contentType});
+      await createMessage({ senderId, roomId, content, contentType });
 
       channel.ack(msg);
-
     } catch (error) {
-      logger.error(error)
-      channel.nack(msg, false, (error instanceof AppError))
+      logger.error(error);
+      channel.nack(msg, false, error instanceof AppError);
     }
-  })
-
-
-};
+  });
+}
 
 export async function stopConsumer() {
-  await channel?.close()
-  await connection?.close()
-};
+  await channel?.close();
+  await connection?.close();
+}
